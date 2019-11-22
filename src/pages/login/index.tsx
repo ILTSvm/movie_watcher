d259290cb318dba2ce1e3767c291b1d49bec56bc
wgz <wangguozhang@mioji.com>
2019-11-22 17:55:37 +0800

    state props

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,25 +1,39 @@
 import axios from 'axios';
 import * as React from 'react';
 import './index.scss';
+import { ILoginProps } from './props';
+import { ILoginState } from './state';
 
-class Login extends React.Component{
-  constructor( props : any ){
+class Login extends React.Component<ILoginProps, ILoginState> {
+  constructor( props : ILoginProps ){
     super(props);
+    this.state = {
+      password:'',
+      username: '',
+    }
   }
 
   // login
-  public login(){
+  public login() {
     axios.post('/api/user/login');
   }
 
-  public register(){
+  public register() {
     axios.post('/api/user/register',{username:'abc',password:'abc'});
   }
-
+  public handleChange(e: any, key: string) {
+    console.log(e.currentTarget.value)
+    this.setState({
+      [key]: e.currentTarget.value,
+    })
+  }
   public render() {
     return (
       <div>
-        <input className = "acc" />
+        <input 
+          className = "acc" 
+          onChange = {(e)=>this.handleChange(e,'username')}
+        />
         <input type = "password" className = "pwd" />
         <button className = "submit" onClick = {this.login}>登录</button>
         <button className = "submit" onClick = {this.register}>注册</button>
